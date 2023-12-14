254d7da5971fc82ccc4c31e3a29ffbf9cd95e854
Flopiras <Flopiras@yahoo.it>
2023-12-14 14:48:14 +0100

    added props for index page

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -4,13 +4,27 @@ import axios from 'axios';
 import { onMounted, ref } from 'vue';
 
 import PizzaIndex from './components/PizzaIndex.vue';
+
+// datas
+const pizzas = ref(null);
+
+// functions
+const getPizzas = async () => {
+
+  const data = await axios.get("http://localhost:8080/api/pizzas");
+
+  pizzas.value = data.data;
+}
+
+//mounted
+onMounted(getPizzas);
 </script>
 
 <template>
   <div class="container">
 
     <!-- index -->
-    <pizzaIndex />
+    <pizzaIndex :pizzas="pizzas" />
 
   </div>
 </template>
--- front-end/src/components/PizzaIndex.vue
+++ front-end/src/components/PizzaIndex.vue
@@ -1,5 +1,18 @@
-<script></script>
+<script setup>
+import { defineProps } from 'vue';
+
+// props
+const props = defineProps({
+    pizzas: {
+        type: Array,
+        required: true
+    }
+});
+</script>
 
 <template>
-    test
+    <h1 class="my-4 text-center">Le nostre pizze</h1>
+    <ul>
+        <li v-for="pizza in pizzas" :key="pizza.id"> {{ pizza.name }}</li>
+    </ul>
 </template>
