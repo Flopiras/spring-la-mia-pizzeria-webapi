58337822b7665a38f21343b4bf2c7fa593d5bcf4
Flopiras <Flopiras@yahoo.it>
2023-12-14 18:34:00 +0100

    added method to create

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -5,11 +5,13 @@ import { onMounted, ref } from 'vue';
 
 import PizzaIndex from './components/PizzaIndex.vue';
 import PizzaShow from './components/PizzaShow.vue';
+import PizzaForm from './components/PizzaForm.vue';
 
 
 // datas
 const pizzas = ref(null);
 const selectedPizza = ref(null);
+const creatingPizza = ref(false);
 
 // functions
 const getPizzas = async () => {
@@ -17,7 +19,7 @@ const getPizzas = async () => {
   const data = await axios.get("http://localhost:8080/api/pizzas");
 
   pizzas.value = data.data;
-}
+};
 
 const showPizza = (id) => {
   pizzas.value.forEach((pizza) => {
@@ -29,11 +31,23 @@ const showPizza = (id) => {
 
   });
   console.log('pizza cliccata');
-}
+};
 
 const closeShow = (update) => {
   selectedPizza.value = null;
   if (update) getPizzas();
+};
+
+const createdPizza = () => {
+
+  creatingPizza.value = false;
+
+  getPizzas();
+};
+
+const deletePizza = () => {
+  selectedPizza.value = null;
+  updatePizzas();
 };
 
 //mounted
@@ -43,11 +57,25 @@ onMounted(getPizzas);
 <template>
   <div class="container">
 
-    <!-- index -->
-    <pizzaIndex v-if="!selectedPizza" :pizzas="pizzas" @show-pizza="showPizza" />
+    <!-- create button -->
+    <div v-if="!creatingPizza && selectedPizza == null" @click="creatingPizza = true"
+      class="d-flex justify-content-center my-4">
+      <button class="btn btn-success">
+        Aggiungi pizza
+      </button>
+    </div>
 
-    <!-- show -->
-    <pizzaShow v-if="selectedPizza" :pizza="selectedPizza" @close-show="closeShow" />
+    <!-- create form -->
+    <pizzaForm v-if="creatingPizza" @back="creatingPizza = false" @saved="createdPizza" />
+
+    <div v-else>
+
+      <!-- index -->
+      <pizzaIndex v-if="!selectedPizza" :pizzas="pizzas" @show-pizza="showPizza" />
+
+      <!-- show -->
+      <pizzaShow v-else :pizza="selectedPizza" @close-show="closeShow" @delete-pizza="deletePizza" />
+    </div>
 
   </div>
 </template>
